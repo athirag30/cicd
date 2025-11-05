7e14934f4e70593df37120cb335d0cd4e3a0287f
athirag30 <athirag3011@gmail.com>
2025-11-05 20:32:39 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from our CI/CD Pipeline! This is version 1.');
+  res.send('Hello from our CI/CD Pipeline! This is version 2.');
 });
 
 app.listen(PORT, () => {
